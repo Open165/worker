d97649f169bee5ecd3566bf18f5c6b51a31f2dbd
MrOrz <johnsonliang7@gmail.com>
2025-05-18 21:57:12 +0800

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ import { screenshotHandler } from './handlers/screenshot';
 import { whoisHandler } from './handlers/whois';
 
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+  async fetch(request: Request, env: Env): Promise<Response> {
     const { pathname } = new URL(request.url);
     switch (pathname) {
       case '/screenshot':
-        return screenshotHandler(request, env, ctx);
+        return screenshotHandler(request, env);
       case '/whois':
-        return whoisHandler(request, env, ctx);
+        return whoisHandler(request);
       default:
         return new Response('Not found', { status: 404 });
     }
-  }
+  },
 } satisfies ExportedHandler<Env>;
